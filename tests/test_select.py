3788929bc11913f228087ecb1eff4ad3c5485239
Andrey <phase8th@gmail.com>
2024-07-07 02:18:28 +0300

    fix

--- tests/test_select.py
+++ tests/test_select.py
@@ -2,7 +2,7 @@ from playwright.sync_api import expect
 
 
 def test_dropdown_selector(page) -> None:
-    page.goto("https://avtodom.ru/actions/?city=msk")
+    page.goto("https://avtodom.ru/actions")
     page.get_by_role("button", name="Все дилеры").click()
     page.get_by_role("button", name="BMW Зорге").click()
     expect(page.locator("[id=\"__nuxt\"]")).to_contain_text("BMW Зорге")
