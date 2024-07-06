d806605dc7da4f4a23c7b726df5575365253fb33
Andrey <phase8th@gmail.com>
2024-07-07 02:12:55 +0300

    fix

--- tests/test_mainpage_search.py
+++ tests/test_mainpage_search.py
@@ -1,5 +1,5 @@
 def test_mainpage_search(page) -> None:
-    page.goto("https://avtodom.ru/")
+    page.goto("https://avtodom.ru")
     page.get_by_placeholder("от 1 594 900 ₽").click()
     page.get_by_placeholder("от 1 594 900 ₽").fill("10000000")
     page.get_by_role("button", name="Показать").click()
